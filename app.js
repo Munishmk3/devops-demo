d8aa67fe9c42b1c5eca6df7e4408b87b87570bd0
Munishmk3 <muneeswaran.mk3@gmail.com>
2026-02-05 10:48:39 +0530

    update jsfile

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const http = require('http');
-http.createServer((req,res)=>{
-  res.end("DevOps Demo App Running");
+const client = require('prom-client');
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+http.createServer((req, res) => {
+  if (req.url === '/metrics') {
+    res.setHeader('Content-Type', client.register.contentType);
+    res.end(client.register.metrics());
+  } else {
+    res.end("DevOps Demo App Running");
+  }
 }).listen(3000);
